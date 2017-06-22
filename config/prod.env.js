378b662d67040ddbd097badbfde85f757cc79d13
Joonhyeok Im <itanoss@gmail.com>
2017-06-22 15:22:22 +0900

    Fix ajax problem(mixed content)
    - Change endpoint uri from http to https.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URI_PURCHASE: '"http://sdsact-msa-pilot-sales-api.herokuapp.com"'
+  BASE_URI_PURCHASE: '"https://sdsact-msa-pilot-sales-api.herokuapp.com"'
 }
